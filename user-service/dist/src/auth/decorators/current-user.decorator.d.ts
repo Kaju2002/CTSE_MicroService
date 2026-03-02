c975713883537feed422f533bdf07b3ef10d633d
SANTA <140438689+it22113740@users.noreply.github.com>
2026-03-02 08:40:45 +0530

    Delete user-service/dist directory

--- user-service/dist/src/auth/decorators/current-user.decorator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const CurrentUser: (...dataOrPipes: ("id" | "email" | "password" | "firstName" | "lastName" | "phone" | "address" | "imageUrl" | "role" | "isActive" | "createdAt" | "updatedAt" | import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>> | undefined)[]) => ParameterDecorator;
--- user-service/dist/src/auth/dto/register.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class RegisterDto {
-    email: string;
-    firstName?: string;
-    lastName?: string;
-    phone?: string;
-    address?: string;
-    imageUrl?: string;
-    password: string;
-}
